d5b7a0f7708f464f83b6165d2ccd28ef185930e4
Freesia <freesia@studiofreesia.com>
2020-01-12 19:15:02 +0900

    デバッグ環境構築

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -39,7 +39,7 @@ namespace Server
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
