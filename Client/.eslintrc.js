09248cd8b61366a33392d0639d8166c65da76076
Freesia <freesia@studiofreesia.com>
2020-01-13 15:15:54 +0900

    設定変更

--- Client/.eslintrc.js
+++ Client/.eslintrc.js
@@ -3,12 +3,32 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/essential", "@vue/prettier", "@vue/typescript"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:prettier/recommended",
+    "plugin:vue/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
+    "@vue/prettier",
+    "@vue/typescript"
+  ],
+  plugins: ["@typescript-eslint", "prettier", "vue"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    "require-atomic-updates": "off", 
+    "@typescript-eslint/indent": ["error", 2],
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/no-explicit-any": "off", // 出来れば対応したい
+    "@typescript-eslint/explicit-member-accessibility": ["error", { accessibility: "no-public" }]
   },
   parserOptions: {
-    parser: "@typescript-eslint/parser"
+    parser: "@typescript-eslint/parser",
+    // 遅い
+    // project: './tsconfig.json',
+    ecmaVersion: 2020,
+    sourceType: "module"
   }
 };
--- Client/vue.config.js
+++ Client/vue.config.js
@@ -1,3 +1,24 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const HardSourceWebpackPlguin = require("hard-source-webpack-plugin");
+
 module.exports = {
-  transpileDependencies: ["vuetify"]
-};
+  transpileDependencies: ["vuetify"],
+  configureWebpack: {
+    devtool: "source-map",
+    plugins: [new HardSourceWebpackPlguin()]
+  },
+  pwa: {
+    name: "Vivideo",
+    themeColor: "#FF9800",
+    msTileColor: "#FF9800",
+    workboxOptions: {
+      clientsClaim: true,
+      skipWaiting: true
+    },
+    // iconPaths: {
+    //   appleTouchIcon: "img/icons/icon-152x152_light.png",
+    //   maskIcon: "img/Logo_light.svg",
+    //   msTileImage: "img/icons/icon-144x144_dark.png"
+    // }
+  }
+}
