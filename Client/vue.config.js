3e5d93d6779f8f16ce620bd3036699316472429e
Freesia <freesia@studiofreesia.com>
2020-01-13 16:16:51 +0900

    クライアントのデバッグ追加

--- Client/vue.config.js
+++ Client/vue.config.js
@@ -3,6 +3,16 @@ const HardSourceWebpackPlguin = require("hard-source-webpack-plugin");
 
 module.exports = {
   transpileDependencies: ["vuetify"],
+  devServer: {
+    proxy: {
+      "/hangfire": {
+        target: "http://localhost:5000"
+      },
+      "/api": {
+        target: "http://localhost:5000"
+      }
+    }
+  },
   configureWebpack: {
     devtool: "source-map",
     plugins: [new HardSourceWebpackPlguin()]
@@ -14,11 +24,11 @@ module.exports = {
     workboxOptions: {
       clientsClaim: true,
       skipWaiting: true
-    },
+    }
     // iconPaths: {
     //   appleTouchIcon: "img/icons/icon-152x152_light.png",
     //   maskIcon: "img/Logo_light.svg",
     //   msTileImage: "img/icons/icon-144x144_dark.png"
     // }
   }
-}
+};
