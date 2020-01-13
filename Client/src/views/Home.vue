b5ea942954272fd8b750dcb4d7677a7070a7defc
Freesia <freesia@studiofreesia.com>
2020-01-13 18:50:23 +0900

    不要な実装整理

--- Client/src/views/Home.vue
+++ Client/src/views/Home.vue
@@ -1,17 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+  <div>
     <Player path="/stream/-812461457" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import Player from "@/components/Player.vue";
-import HelloWorld from "@/components/HelloWorld.vue";
 import { Component } from "vue-property-decorator";
 
-@Component({ components: { HelloWorld, Player } })
+@Component({ components: { Player } })
 export default class Home extends Vue {}
 </script>
