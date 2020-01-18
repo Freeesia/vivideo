5144d85d3fd0ab3db8f82cf9414e8e70fa1469c9
Freesia <freesia@studiofreesia.com>
2020-01-18 18:12:16 +0900

    トランスコード設定も外部から設定できるように変更

---
+++ Worker/Model/TranscodeSetting.cs
@@ -0,0 +1,8 @@
+namespace StudioFreesia.Vivideo.Worker.Model
+{
+    public class TranscodeSetting
+    {
+        public string? File { get; set; }
+        public string? Args { get; set; }
+    }
+}
--- Worker/TranscodeVideoImpl.cs
+++ Worker/TranscodeVideoImpl.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using StudioFreesia.Vivideo.Core;
+using StudioFreesia.Vivideo.Worker.Model;
 
 namespace StudioFreesia.Vivideo.Worker
 {
@@ -17,12 +18,18 @@ namespace StudioFreesia.Vivideo.Worker
         private readonly string inputDir;
         private readonly string execDir;
 
+        private readonly string file;
+        private readonly string args;
+
         public TranscodeVideoImpl(IConfiguration config, ILogger<TranscodeVideoImpl> logger)
         {
             this.logger = logger;
             var content = config.GetSection("Content").Get<ContentDirSetting>();
             this.workDir = content?.Work ?? throw new ArgumentException();
             this.inputDir = content?.List ?? throw new ArgumentException();
+            var trans = config.GetSection("Transcode").Get<TranscodeSetting>();
+            this.file = trans.File ?? throw new ArgumentException();
+            this.args = trans.Args ?? throw new ArgumentException();
             this.execDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? throw new InvalidOperationException();
         }
 
@@ -37,33 +44,23 @@ namespace StudioFreesia.Vivideo.Worker
             }
             Directory.CreateDirectory(dir);
             var input = Path.IsPathRooted(queue.Input) ? queue.Input : Path.Combine(this.inputDir, queue.Input);
-            var info = new ProcessStartInfo(@"ffmpeg\bin\ffmpeg.exe")
+            var info = new ProcessStartInfo(this.file)
             {
                 CreateNoWindow = true,
                 UseShellExecute = false,
                 WorkingDirectory = this.execDir,
                 RedirectStandardError = true,
-                ArgumentList = {
-                    "-hide_banner",
-                    "-v", "warning",
-                    "-i", input.Replace('\\', '/'),
-                    "-c:v", "h264_nvenc",
-                    "-c:a", "copy",
-                    "-window_size", "0",
-                    "-hls_playlist", "1",
-                    "-movflags", "+faststart",
-                    Path.Combine(dir, "master.mpd").Replace('\\', '/'),
-                }
+                Arguments = string.Format(this.args, input.Replace('\\', '/'), Path.Combine(dir, "master.mpd").Replace('\\', '/')),
             };
             this.logger.LogInformation("トランスコード開始:{0}", name);
             using var p = Process.Start(info);
             p.ErrorDataReceived += (s, e) =>
-            {
-                if (!string.IsNullOrEmpty(e.Data))
                 {
-                    this.logger.LogError(e.Data);
-                }
-            };
+                    if (!string.IsNullOrEmpty(e.Data))
+                    {
+                        this.logger.LogError(e.Data);
+                    }
+                };
             p.BeginErrorReadLine();
             p.WaitForExit();
             this.logger.LogInformation("トランスコード終了:{0}", name);
