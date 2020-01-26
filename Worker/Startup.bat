b39163f1b4d2c1c8c1f2bb00467934f47170e62c
Freesia <freesia@studiofreesia.com>
2020-01-26 20:50:37 +0900

    起動処理バッチ追加

---
+++ Worker/Startup.bat
@@ -0,0 +1,4 @@
+set BASE_DIR=%~dp0
+popd %BASE_DIR%
+Worker.exe
+pushd
--- Worker/install.bat
+++
@@ -1,2 +0,0 @@
-set BASE_DIR=%~dp0
-sc create "Vivideo" start= auto displayname= "Frix TV Prime" binpath= %BASE_DIR%\Worker.exe
