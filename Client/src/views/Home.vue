5a8a95c42999af359d2ebe24271192252630f56e
Freesia <freesia@studiofreesia.com>
2020-01-26 18:27:07 +0900

    リスト表示改善

--- Client/src/views/Home.vue
+++ Client/src/views/Home.vue
@@ -3,18 +3,31 @@
     <v-progress-linear :active="loading" indeterminate />
     <v-container class="fill-height" fluid>
       <v-row>
-        <div>{{ $route.params.request }}</div>
+        <v-col> {{ $route.params.request }} </v-col>
+        <v-col sm="4">
+          <v-text-field
+            v-model="search"
+            label="検索"
+            prepend-inner-icon="search"
+            hide-details
+            clearable
+            dense
+            outlined
+          />
+        </v-col>
       </v-row>
       <v-row>
-        <v-list>
-          <v-list-item-group color="primary">
-            <v-list-item v-for="item in contents" :key="item.name" @click="selectContent(item)">
-              <v-list-item-content>
-                <v-list-item-title>{{ item.name }}</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list-item-group>
-        </v-list>
+        <v-col>
+          <v-list>
+            <v-list-item-group color="primary">
+              <v-list-item v-for="item in filtered" :key="item.name" @click="selectContent(item)">
+                <v-list-item-content>
+                  <v-list-item-title>{{ item.name }}</v-list-item-title>
+                </v-list-item-content>
+              </v-list-item>
+            </v-list-item-group>
+          </v-list>
+        </v-col>
       </v-row>
     </v-container>
   </div>
@@ -31,15 +44,27 @@ import Axios from "axios";
 export default class Home extends Vue {
   private selectedContent: ContentNode | null = null;
   private contents: ContentNode[] = [];
+  private filtered: ContentNode[] = [];
   private loading = true;
+  private search: string | null = null;
 
   @Watch("$route", { immediate: true, deep: true })
   private async onRequestChanged() {
     this.loading = true;
     this.contents = [];
+    this.search = "";
     const res = await Axios.get<ContentNode[]>("/api/video/" + (this.$route.params.request ?? ""));
-    this.contents = res.data;
+    this.filtered = this.contents = res.data;
     this.loading = false;
+  }
+
+  @Watch("search")
+  private onSearchChanged() {
+    if (this.search) {
+      this.filtered = this.contents.filter(n => n.name.toUpperCase().includes(this.search.toUpperCase()));
+    } else {
+      this.filtered = this.contents;
+    }
   }
 
   private selectContent(content: ContentNode) {
