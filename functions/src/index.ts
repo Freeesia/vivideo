933809bf33b6d2d7e6429a46c1f73d309530cb3d
Freesia <freesia@studiofreesia.com>
2020-02-02 01:06:44 +0900

    とりあえず、招待機能写経

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,69 @@
-import * as functions from 'firebase-functions';
+import { https } from "firebase-functions";
+import { HttpsError } from "firebase-functions/lib/providers/https";
+import { firestore } from "firebase-admin";
+import admin = require("firebase-admin");
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const invitationCodes = "invitationCodes";
+
+export const checkInvitationCode = https.onCall(async (data, _) => {
+  if (!data.invitationCode) {
+    throw new HttpsError("invalid-argument", "auth/operation-not-allowed");
+  }
+
+  const codeRef = await firestore()
+    .collection(invitationCodes)
+    .doc(data.invitationCode)
+    .get();
+
+  const code = codeRef.data();
+  if (!code) {
+    throw new HttpsError("invalid-argument", "auth/invalid-code");
+  }
+
+  const count: number = code.remainingCount;
+  if (count <= 0) {
+    throw new HttpsError("invalid-argument", "auth/code-already-in-use");
+  }
+
+  return { status: "ok" };
+});
+
+export const signup = https.onCall(async (data, _) => {
+  if (!data.email || !data.password || !data.name || !data.invitationCode) {
+    throw new HttpsError("invalid-argument", "auth/operation-not-allowed");
+  }
+
+  const codeDoc = firestore()
+    .collection(invitationCodes)
+    .doc(data.invitationCode);
+
+  const user = await admin.firestore().runTransaction(async trans => {
+    const codeRef = await trans.get(codeDoc);
+    const code = codeRef.data();
+    if (!code) {
+      throw new HttpsError("invalid-argument", "auth/invalid-code");
+    }
+    const count: number = code.remainingCount;
+    if (count <= 0) {
+      throw new HttpsError("invalid-argument", "auth/code-already-in-use");
+    }
+    trans.update(codeDoc, {
+      remainingCount: firestore.FieldValue.increment(-1)
+    });
+
+    return admin.auth().createUser({
+      email: data.email,
+      emailVerified: false,
+      password: data.password,
+      displayName: data.name
+    });
+  });
+
+  if (user) {
+    await admin.auth().setCustomUserClaims(user.uid, { invitationCodeVerified: true });
+  }
+
+  console.log({ userId: user.uid, code: data.invitationCode });
+
+  return { status: "ok" };
+});
