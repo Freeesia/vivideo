b6259b6260e40cc325df51afc7d91f0083108c4a
Freesia <freesia@studiofreesia.com>
2020-02-01 21:05:38 +0900

    クライアントにfirebaseついあk

---
+++ Client/src/firebase.ts
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+
+// Initialize Firebase
+firebase.initializeApp({
+  apiKey: "AIzaSyCwqZqUUMbE6n5T3gdMFrLNW30VYvrKh20",
+  authDomain: "vivideo.firebaseapp.com",
+  databaseURL: "https://vivideo.firebaseio.com",
+  projectId: "vivideo",
+  storageBucket: "vivideo.appspot.com",
+  messagingSenderId: "313434128507",
+  appId: "1:313434128507:web:8246d5fac382681b6962de",
+  measurementId: "G-TE802BVQ2K"
+});
+firebase.analytics();
