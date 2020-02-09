111928add73363957cdc2150f7787d6966b8498e
Freesia <freesia@studiofreesia.com>
2020-02-10 02:31:42 +0900

    動画のサムネイル取得処理追加

--- Server/Controllers/ThumbnailController.cs
+++ Server/Controllers/ThumbnailController.cs
@@ -1,12 +1,15 @@
 using System;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using StudioFreesia.Vivideo.Core;
 using Microsoft.AspNetCore.Http;
 using System.IO;
 using System.Linq;
+using StudioFreesia.Vivideo.Server.Model;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Hosting;
+using System.Diagnostics;
 
 namespace StudioFreesia.Vivideo.Server.Controllers
 {
@@ -15,11 +18,23 @@ namespace StudioFreesia.Vivideo.Server.Controllers
     public class ThumbnailController : ControllerBase
     {
         private readonly string contentDir;
+        private readonly string file;
+        private readonly string args;
+        private readonly string tmpDir;
+        private readonly string rootDir;
+        private readonly ILogger<ThumbnailController> logger;
 
-        public ThumbnailController(IConfiguration config)
+        public ThumbnailController(IConfiguration config, IHostEnvironment env, ILogger<ThumbnailController> logger)
         {
             var content = config.GetSection("Content").Get<ContentDirSetting>();
             this.contentDir = content?.List ?? throw new ArgumentException();
+            var trans = config.GetSection("Thumbnail").Get<ThumbnailSetting>();
+            this.file = trans.File ?? throw new ArgumentException();
+            this.args = trans.Args ?? throw new ArgumentException();
+            this.tmpDir = Path.Combine(Path.GetTempPath(), "VivideoThumb");
+            Directory.CreateDirectory(this.tmpDir);
+            this.rootDir = env.ContentRootPath;
+            this.logger = logger;
         }
 
         [HttpGet("{*path}")]
@@ -35,9 +50,45 @@ namespace StudioFreesia.Vivideo.Server.Controllers
                 {
                     return NotFound();
                 }
-                return PhysicalFile(logo, "image/" + Path.GetExtension(logo));
+                return PhysicalFile(logo, "image/" + Path.GetExtension((ReadOnlySpan<char>)logo).Slice(1).ToString());
+            }
+            else if (System.IO.File.Exists(fullPath))
+            {
+                var buf = await Task.Run(async () => await CreateVideoThumbnail(fullPath)).ConfigureAwait(false);
+                return File(buf, "image/png");
             }
             return NotFound();
+        }
+
+        private async Task<byte[]> CreateVideoThumbnail(string path)
+        {
+            var name = Path.GetFileNameWithoutExtension(path);
+            var tmp = Path.Combine(this.tmpDir, Path.GetRandomFileName() + ".png");
+            var info = new ProcessStartInfo(this.file)
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                WorkingDirectory = this.rootDir,
+                RedirectStandardError = true,
+                Arguments = string.Format(this.args, path.Replace('\\', '/'), tmp.Replace('\\', '/')),
+            };
+            this.logger.LogDebug("サムネイル生成開始:{0}", name);
+            using var p = Process.Start(info);
+            p.ErrorDataReceived += (s, e) =>
+                {
+                    if (!string.IsNullOrEmpty(e.Data))
+                    {
+                        this.logger.LogError(e.Data);
+                    }
+                };
+            p.BeginErrorReadLine();
+            p.WaitForExit();
+            this.logger.LogDebug("サムネイル生成終了:{0}", name);
+            if (!System.IO.File.Exists(tmp))
+            {
+                throw new Exception($"「{name}」のサムネイル出力に失敗しました");
+            }
+            return await System.IO.File.ReadAllBytesAsync(tmp).ConfigureAwait(false);
         }
     }
 }
---
+++ Server/Model/ThumbnailSetting.cs
@@ -0,0 +1,8 @@
+namespace StudioFreesia.Vivideo.Server.Model
+{
+    public class ThumbnailSetting
+    {
+        public string? File { get; set; }
+        public string? Args { get; set; }
+    }
+}
