8cb83ae15bc705cf785ee4dd581d24f8bd87db59
Freesia <freesia@studiofreesia.com>
2020-03-08 20:22:18 +0900

    自動再生の実装

--- Client/src/views/Play.vue
+++ Client/src/views/Play.vue
@@ -2,12 +2,23 @@
   <v-container fluid>
     <v-row no-gutters dense>
       <v-col cols="12" md="8">
-        <Player :stream-path="streamPath" :thumbnail-path="thumbnailPath" />
+        <Player :stream-path="streamPath" :thumbnail-path="thumbnailPath" @ended="ended">
+          <template v-if="autoNext" v-slot:overlay>
+            <v-row justify="center">
+              <v-progress-circular class="ma-2" rotate="-90" size="60" :value="percent"></v-progress-circular>
+            </v-row>
+            <v-btn @click="cancelAuto">キャンセル</v-btn>
+          </template>
+        </Player>
         <div>{{ path }}</div>
       </v-col>
       <v-col md="4">
         <v-list>
-          <v-subheader>プレイリスト</v-subheader>
+          <v-subheader>
+            プレイリスト
+            <v-spacer></v-spacer>
+            <v-switch v-model="autoNext" label="自動再生"></v-switch>
+          </v-subheader>
           <v-list-item v-for="item in contents" :key="item.name" @click="select(item)">
             <v-list-item-avatar tile>
               <v-overlay absolute :value="isPlaying(item)">
@@ -42,11 +53,14 @@ import path from "path";
 import assert from "assert";
 import { assertIsDefined } from "../utilities/assert";
 import { getThumbnailPath } from "../utilities/pathUtility";
+import { delay } from "../utilities/systemUtility";
 
 @Component({ components: { Player } })
 export default class Play extends Vue {
   private streamPath = "";
   private thumbnailPath = "";
+  private autoNext = true;
+  private percent = 0;
 
   @Prop({ required: true, type: String, default: "" })
   path!: string;
@@ -83,13 +97,42 @@ export default class Play extends Vue {
     return this.path === item.contentPath;
   }
 
-  private select(item: ContentNode) {
-    this.$router.push({
+  private async ended() {
+    const index = this.contents.findIndex(c => c.contentPath === this.path);
+    if (index < 0 || this.contents.length - 1 <= index) {
+      this.autoNext = false;
+    }
+    if (!this.autoNext) {
+      return;
+    }
+    const next = this.contents[index + 1];
+    assertIsDefined(this.axios);
+    // ローカルだと待っている間に初期セグメント超えて次のセグメントから再生してしまう
+    this.axios.post<string>(`/api/video/transcode/?path=${encodeURIComponent(next.contentPath)}`);
+    const now = Date.now();
+    this.percent = 0;
+    while (this.percent < 100) {
+      this.percent = ((Date.now() - now) / 3000) * 100;
+      await delay(300);
+      if (!this.autoNext) {
+        return;
+      }
+    }
+    this.select(next);
+  }
+
+  private cancelAuto() {
+    this.autoNext = false;
+  }
+
+  private async select(item: ContentNode) {
+    await this.$router.push({
       name: "play",
       query: {
         path: item.contentPath
       }
     });
+    this.loadVideo();
   }
 }
 </script>
