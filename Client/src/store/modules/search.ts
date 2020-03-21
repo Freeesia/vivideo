e0109e87e4413b0c60bc91d9686657b1d4cb00ef
Freesia <freesia@studiofreesia.com>
2020-03-22 00:43:13 +0900

    不要な処理削除

--- Client/src/store/modules/search.ts
+++ Client/src/store/modules/search.ts
@@ -19,29 +19,11 @@ export interface SortOrder {
 @Module({ namespaced: true, name: "search" })
 export default class Search extends VuexModule {
   filter = "";
-  sort = SortType.Name;
-  order = OrderType.Asc;
   sorts: SortOrder[] = [];
 
   @Mutation
   setFilter(value: string) {
     this.filter = value;
-  }
-
-  @Mutation
-  setSort(value: SortType) {
-    this.sort = value;
-  }
-
-  @Mutation
-  setOrder(value: OrderType) {
-    this.order = value;
-  }
-
-  @Mutation
-  setSortAndOrder(value: { sort: SortType; order: OrderType }) {
-    this.sort = value.sort;
-    this.order = value.order;
   }
 
   @Mutation
