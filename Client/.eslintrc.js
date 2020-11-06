1cfea6a3fde143855fc9a5262562ac1046b1950e
Freesia <freesia@studiofreesia.com>
2020-11-07 01:30:37 +0900

    lint設定追加

--- Client/.eslintrc.js
+++ Client/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
   extends: [
     "eslint:recommended",
@@ -12,23 +12,36 @@ module.exports = {
     "prettier",
     "prettier/@typescript-eslint",
     "@vue/prettier",
-    "@vue/typescript"
+    "@vue/typescript",
   ],
   plugins: ["@typescript-eslint", "prettier", "vue"],
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
     "require-atomic-updates": "off",
+    "comma-dangle": [
+      "warn",
+      {
+        arrays: "always-multiline",
+        objects: "always-multiline",
+        imports: "always-multiline",
+        exports: "always-multiline",
+        functions: "only-multiline",
+      },
+    ],
     "@typescript-eslint/indent": ["error", 2],
     "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/explicit-module-boundary-types": "off",
     "@typescript-eslint/no-explicit-any": "off", // 出来れば対応したい
-    "@typescript-eslint/explicit-member-accessibility": ["error", { accessibility: "no-public" }]
+    "@typescript-eslint/explicit-member-accessibility": ["error", { accessibility: "no-public" }],
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-empty-interface": "off",
   },
   parserOptions: {
     parser: "@typescript-eslint/parser",
     // 遅い
     // project: './tsconfig.json',
     ecmaVersion: 2020,
-    sourceType: "module"
-  }
+    sourceType: "module",
+  },
 };
