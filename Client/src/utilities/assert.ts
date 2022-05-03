d75ef207106d2ee7609c74eebfe767a19367bd45
Freesia <freesia@studiofreesia.com>
2022-05-04 04:46:42 +0900

    とりあえずビルド通った

--- Client/src/utilities/assert.ts
+++ Client/src/utilities/assert.ts
@@ -1,7 +1,5 @@
-import { AssertionError } from "assert";
-
 export function assertIsDefined<T>(val: T): asserts val is NonNullable<T> {
   if (val === undefined || val === null) {
-    throw new AssertionError({ message: `Expected 'val' to be defined, but received ${val}` });
+    throw new Error(`Expected 'val' to be defined, but received ${val}`);
   }
 }
---
+++ Client/vite.config.ts
@@ -0,0 +1,44 @@
+import { defineConfig } from "vite";
+import { createVuePlugin } from "vite-plugin-vue2";
+import Components from "unplugin-vue-components/vite";
+import { VuetifyResolver } from "unplugin-vue-components/resolvers";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": __dirname + "/src",
+    },
+  },
+  plugins: [
+    createVuePlugin(),
+    Components({
+      resolvers: [VuetifyResolver()],
+      dts: "src/components.d.ts",
+    }),
+  ],
+  build: {
+    chunkSizeWarningLimit: 1500,
+  },
+  css: {
+    // sassのバージョン上げれば動くけど、Vuetifyで警告が出る…
+    // preprocessorOptions: {
+    //   sass: { charset: false },
+    //   scss: { charset: false },
+    // },
+    postcss: {
+      plugins: [
+        {
+          postcssPlugin: "internal:charset-removal",
+          AtRule: {
+            charset: atRule => {
+              if (atRule.name === "charset") {
+                atRule.remove();
+              }
+            },
+          },
+        },
+      ],
+    },
+  },
+});
