bea4a4ebff41014514c541f5f17172a6be740183
Freesia <freesia@studiofreesia.com>
2022-05-06 03:29:45 +0900

    PWAの設定

--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import { createVuePlugin } from "vite-plugin-vue2";
+import { VitePWA } from "vite-plugin-pwa";
 import Components from "unplugin-vue-components/vite";
 import { VuetifyResolver } from "unplugin-vue-components/resolvers";
 import NodeModulesPolyfills from "@esbuild-plugins/node-modules-polyfill";
@@ -25,6 +26,50 @@ export default defineConfig(env => ({
       resolvers: [VuetifyResolver()],
       dts: "src/components.d.ts",
     }),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "robots.txt"],
+      manifest: {
+        name: "Frix TV Prime",
+        theme_color: "#FF9800",
+        lang: "ja",
+      },
+      workbox: {
+        maximumFileSizeToCacheInBytes: 3 * 1024 * 1024,
+        runtimeCaching: [
+          {
+            urlPattern: /\/api\/thumbnail\//,
+            handler: "StaleWhileRevalidate",
+            method: "GET",
+            options: {
+              cacheName: "thumbnail-cache",
+              expiration: {
+                maxEntries: 1000,
+                maxAgeSeconds: 60 * 60 * 24 * 365,
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+          {
+            urlPattern: /\/api\/video\//,
+            handler: "StaleWhileRevalidate",
+            method: "GET",
+            options: {
+              cacheName: "list-cache",
+              expiration: {
+                maxEntries: 1000,
+                maxAgeSeconds: 60 * 60 * 6,
+              },
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      },
+    }),
   ],
   server: {
     proxy: {
@@ -38,7 +83,7 @@ export default defineConfig(env => ({
     },
   },
   build: {
-    chunkSizeWarningLimit: 1500,
+    chunkSizeWarningLimit: 3000,
     rollupOptions: {
       plugins: [rollupNodePolyFill()],
     },
