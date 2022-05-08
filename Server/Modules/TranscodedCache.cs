c451b7f3d09d712e4208574d4d3e51baa1bd75f8
Freesia <freesia@studiofreesia.com>
2022-05-08 16:32:07 +0900

    トランスコードキャッシュの期限設定

--- Server/Modules/TranscodedCache.cs
+++ Server/Modules/TranscodedCache.cs
@@ -1,12 +1,12 @@
-using Microsoft.AspNetCore.Mvc.Filters;
 using Nito.AsyncEx;
 using StackExchange.Redis;
+using StackExchange.Redis.KeyspaceIsolation;
 
 namespace StudioFreesia.Vivideo.Server.Modules;
 
 public class TranscodedCache : ITranscodedCache
 {
-    private static readonly RedisKey KeyPrefix = nameof(TranscodedCache) + ":";
+    private static readonly TimeSpan expiry = TimeSpan.FromDays(30);
     private readonly string connectionString;
     private readonly AsyncLazy<ConnectionMultiplexer> con;
 
@@ -24,31 +24,33 @@ public class TranscodedCache : ITranscodedCache
     private async Task<IDatabase> GetDatabase()
     {
         var con = await this.con;
-        return con.GetDatabase();
+        return con.GetDatabase().WithKeyPrefix(nameof(TranscodedCache) + ":");
     }
 
     public async Task<ReadOnlyMemory<byte>> Get(string key, string file)
     {
         var db = await GetDatabase();
-        return await db.HashGetAsync(KeyPrefix.Append(key), file);
+        var lease = await db.HashGetLeaseAsync(key, file);
+        return lease.Memory;
     }
 
     public async Task Set(string key, string file, ReadOnlyMemory<byte> buf)
     {
         var db = await GetDatabase();
-        await db.HashSetAsync(KeyPrefix.Append(key), file, buf);
+        await db.HashSetAsync(key, file, buf);
+        await db.KeyExpireAsync(key, expiry);
     }
 
     public async Task<bool> Exist(string key)
     {
         var db = await GetDatabase();
-        return await db.KeyExistsAsync(KeyPrefix.Append(key));
+        return await db.KeyExistsAsync(key);
     }
 
     public async Task Delete(string key)
     {
         var db = await GetDatabase();
-        await db.KeyDeleteAsync(KeyPrefix.Append(key));
+        await db.KeyDeleteAsync(key);
     }
 }
 
