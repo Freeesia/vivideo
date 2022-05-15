16d2a501637a421b826ad87a994ee5cfe1392371
Freesia <freesia@studiofreesia.com>
2022-05-15 16:42:52 +0900

    sentryの設定更新

--- Client/src/main.ts
+++ Client/src/main.ts
@@ -4,9 +4,8 @@ import "./registerServiceWorker";
 import "./firebase";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
+import "@/plugins/sentry";
 import { store } from "./store";
-import { init as SentryInit } from "@sentry/browser";
-import { Vue as SentryVue } from "@sentry/integrations";
 import VuetifyDialog from "vuetify-dialog";
 import "vuetify-dialog/dist/vuetify-dialog.css";
 
@@ -17,15 +16,6 @@ Vue.use(VuetifyDialog, {
     vuetify,
   },
 });
-
-if (process.env.NODE_ENV === "production") {
-  SentryInit({
-    dsn: "https://6bd5217ab2e24414973357727d9df261@sentry.io/2409801",
-    release: import.meta.env.VITE_APP_VERSION,
-    environment: "Production",
-    integrations: [new SentryVue({ Vue, attachProps: true })],
-  });
-}
 
 new Vue({
   router,
---
+++ Client/src/plugins/sentry.ts
@@ -0,0 +1,21 @@
+import Vue from "vue";
+import * as Sentry from "@sentry/vue";
+import { BrowserTracing } from "@sentry/tracing";
+import router from "@/router";
+
+if (import.meta.env.PROD) {
+  Sentry.init({
+    Vue,
+    dsn: "https://6bd5217ab2e24414973357727d9df261@o351180.ingest.sentry.io/2409801",
+    integrations: [
+      new BrowserTracing({
+        routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+      }),
+    ],
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+    release: import.meta.env.VITE_APP_VERSION,
+  });
+}
