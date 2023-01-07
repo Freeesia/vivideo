7a990cb791e5e988b613f7991af91918b0b73980
Freesia <freesia@studiofreesia.com>
2023-01-07 11:09:42 +0000

    ffmpegもインストール

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,3 +1,5 @@
 sudo chown vscode:vscode /workspace/node_modules/
+sudo apt-get update
+sudo apt-get install --no-install-recommends -y ffmpeg
 bash -i -c 'nvm install && yarn'
 mkdir -p ~/.ssh && cp -r ~/.ssh-localhost/* ~/.ssh && chmod 700 ~/.ssh && chmod 600 ~/.ssh/*
