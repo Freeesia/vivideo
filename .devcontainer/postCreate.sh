9fb457d714b376cc04160be395179cf80b426554
Freesia <freesia@studiofreesia.com>
2023-01-22 11:31:34 +0000

    最新のffmpeg使うように変更

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,5 +1,3 @@
 sudo chown vscode:vscode /workspace/node_modules/
-sudo apt-get update
-sudo apt-get install --no-install-recommends -y ffmpeg
 mkdir -p ~/.ssh && cp -r ~/.ssh-localhost/* ~/.ssh && chmod 700 ~/.ssh && chmod 600 ~/.ssh/*
 bash -i -c 'nvm install && yarn'
