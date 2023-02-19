cb0e8131e6c7431d9d10f8d9889f0c571a2888b0
Freesia <freesia@studiofreesia.com>
2023-02-19 15:17:44 +0000

    functionsでエミュレータつなげるように

--- Client/src/firebase/functions.ts
+++ Client/src/firebase/functions.ts
@@ -2,13 +2,12 @@ import { app } from ".";
 import { getFunctions, connectFunctionsEmulator, httpsCallableFromURL } from "firebase/functions";
 
 export const functions = getFunctions(app, "asia-northeast1");
+let createUrl = (func: string) => `https://${func}-uxogjp5z3q-an.a.run.app/${func}`;
 if (import.meta.env.DEV) {
   connectFunctionsEmulator(functions, "localhost", 5010);
+  createUrl = (func: string) => `http://localhost:5010/${functions.app.options.projectId}/${functions.region}/${func}`;
 }
 
-export const invite = httpsCallableFromURL<void, string>(functions, "https://invite-uxogjp5z3q-an.a.run.app/invite");
-export const checkCode = httpsCallableFromURL<{ invitationCode: string }>(
-  functions,
-  "https://checkInvitationCode-uxogjp5z3q-an.a.run.app/checkInvitationCode"
-);
-export const signup = httpsCallableFromURL(functions, "https://signup-uxogjp5z3q-an.a.run.app/signup");
+export const invite = httpsCallableFromURL<void, string>(functions, createUrl("invite"));
+export const checkCode = httpsCallableFromURL<{ invitationCode: string }>(functions, createUrl("checkinvitationcode"));
+export const signup = httpsCallableFromURL(functions, createUrl("signup"));
